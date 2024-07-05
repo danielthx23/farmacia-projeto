8ab2c09747f670e912c78227f2a39f984ca560b2
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-07-05 09:46:17 -0300

    Feat: Models & Navbar-Home-Footer

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import NavBar from '../../components/navbar/NavBar'
+import Footer from '../../components/footer/Footer'
+
+function Home() {
+    return (
+        <>
+         <div className="bg-white flex flex-col justify-center items-center">
+                <div className='container grid grid-cols-2 text-gray-800 p-16'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>Pukemun</h2>
+                        <p className='text-xl'>Pukemano</p>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <img src='https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/195.png' alt="" className='object-fill' />
+
+                    </div>
+                </div>
+                <div className='container grid grid-cols-2  text-gray-800 p-16'>
+                <div className="flex justify-center">
+                        <img src='https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/283.png' alt="" className='object-contain' />
+
+                    </div>
+
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>a</h2>
+                        <p className='text-xl'>a</p>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
